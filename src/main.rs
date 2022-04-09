0255ce0d9acafea7c2b3e448284fdd9ff902b9f0
Lukas1818 <git@lukas1818.de>
2022-04-09 22:11:57 +0200

    use xwallaper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{self, bail};
 use clap::Parser;
 use once_cell::sync::Lazy;
 use rand::{seq::SliceRandom, thread_rng};
@@ -6,8 +7,10 @@ use std::{
 	fs,
 	fs::File,
 	io::{prelude::*, BufReader, Write},
+	process::{exit, Command},
 };
 use wallpaper;
+use xrandr;
 
 mod config;
 
@@ -25,7 +28,7 @@ enum Opt {
 	Set,
 }
 
-fn download() {
+fn download() -> anyhow::Result<()> {
 	let mut image_paths = get_posts(&vec!["rating:safe".to_string()], 10);
 	fs::create_dir(config::WALLPAPERS_FILE.as_path().parent().unwrap());
 	fs::create_dir(config::WALLPAPERS_FOLDER.to_string());
@@ -34,22 +37,47 @@ fn download() {
 		file.write_all(image_paths.pop().unwrap().as_ref());
 		file.write_all(b"\n");
 	}
+	Ok(())
 }
 
-fn set() {
+fn set() -> anyhow::Result<()> {
 	let mut image_paths: Vec<String> = Vec::new();
 	let file = File::open(config::WALLPAPERS_FILE.as_path()).unwrap();
 	let bufreader = BufReader::new(file);
 	for line in bufreader.lines().enumerate() {
 		image_paths.push(line.1.unwrap());
 	}
+	let monitors = xrandr::XHandle::open().unwrap().monitors().unwrap();
+	let mut active_monitors: Vec<String> = Vec::new();
+	for monitor in monitors {
+		if monitor.is_automatic {
+			active_monitors.push(monitor.name);
+		}
+	}
 	let mut rng = thread_rng();
-	wallpaper::set_from_path(image_paths.choose(&mut rng).unwrap());
+	let mut command = Command::new("xwallpaper");
+	for monitor in active_monitors {
+		command.args(["--output", &monitor, "--zoom", image_paths.choose(&mut rng).unwrap()]);
+	}
+	let status = command.spawn();
+	if let Err(error) = status {
+		if error.kind() == std::io::ErrorKind::NotFound {
+			println!("xwallpaper not found, use wallpaper crate as fallback");
+			wallpaper::set_from_path(image_paths.choose(&mut rng).unwrap());
+		} else {
+			bail!("Error: can not set Wallpaper with : {error}")
+		}
+	};
+	Ok(())
 }
 
 fn main() {
-	match Opt::parse() {
+	let result = match Opt::parse() {
 		Opt::Download => download(),
 		Opt::Set => set(),
 	};
+	if let Err(error) = result {
+		eprintln!("{:?}", error);
+		exit(1);
+	}
 }
