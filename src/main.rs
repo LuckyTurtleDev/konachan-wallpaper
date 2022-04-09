d09f1a70128d2e6afb0ab75f6ff531ac2df5342d
Lukas1818 <git@lukas1818.de>
2022-04-09 20:49:19 +0200

    set wallpaper

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use clap::Parser;
 use once_cell::sync::Lazy;
+use rand::{seq::SliceRandom, thread_rng};
 use reqwest::Client;
-use std::{fs, fs::File, io::Write};
+use std::{
+	fs,
+	fs::File,
+	io::{prelude::*, BufReader, Write},
+};
+use wallpaper;
 
 mod config;
 
@@ -9,7 +16,16 @@ use konachan::*;
 
 static CLIENT: Lazy<Client> = Lazy::new(|| Client::new());
 
-fn main() {
+#[derive(Debug, Parser)]
+enum Opt {
+	/// download new pictures
+	Download,
+
+	/// set a dowloaded picture as Wallpaper
+	Set,
+}
+
+fn download() {
 	let mut image_paths = get_posts(&vec!["rating:safe".to_string()], 10);
 	fs::create_dir(config::WALLPAPERS_FILE.as_path().parent().unwrap());
 	fs::create_dir(config::WALLPAPERS_FOLDER.to_string());
@@ -18,4 +34,22 @@ fn main() {
 		file.write_all(image_paths.pop().unwrap().as_ref());
 		file.write_all(b"\n");
 	}
+}
+
+fn set() {
+	let mut image_paths: Vec<String> = Vec::new();
+	let file = File::open(config::WALLPAPERS_FILE.as_path()).unwrap();
+	let bufreader = BufReader::new(file);
+	for line in bufreader.lines().enumerate() {
+		image_paths.push(line.1.unwrap());
+	}
+	let mut rng = thread_rng();
+	wallpaper::set_from_path(image_paths.choose(&mut rng).unwrap());
+}
+
+fn main() {
+	match Opt::parse() {
+		Opt::Download => download(),
+		Opt::Set => set(),
+	};
 }
