ea3a781b8048ad65ccfc7dc292d62a0aa514fca9
msrd0 <msrd0@users.noreply.github.com>
2022-05-05 17:43:56 +0200

    Update konachan.rs

--- src/konachan.rs
+++ src/konachan.rs
@@ -25,7 +25,6 @@ mod serde_tags {
 }
 
 pub async fn download_and_save_image(url: &str, path: &Path) -> anyhow::Result<()> {
-	let path = path.as_ref();
 	let image = CLIENT.get(url).send().await?.bytes().await?;
 	fs::write(path, image).await.unwrap();
 	println!("{}", path.display());
@@ -33,7 +32,7 @@ pub async fn download_and_save_image(url: &str, path: &Path) -> anyhow::Result<(
 }
 
 pub async fn download_and_save_image_retry(url: String, path: impl AsRef<Path>) {
-	let path: &Path = path.as_ref();
+	let path = path.as_ref();
 	loop {
 		match download_and_save_image(&url, path).await {
 			Ok(_) => break,
