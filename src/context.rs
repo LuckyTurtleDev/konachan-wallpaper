208569e1b699e715c0cfda354fe92069a01e9e69
LuckyTurtleDev <git@lukas1818.de>
2022-10-20 19:56:57 +0200

    add router.mac variable

--- src/context.rs
+++ src/context.rs
@@ -1,7 +1,10 @@
 use chrono::{offset::Local, Datelike, Timelike};
 use evalexpr::{context_map, EvalexprError, HashMapContext};
 use local_ip_address::local_ip;
-use std::net::{IpAddr, IpAddr::*, Ipv4Addr};
+use std::{
+	net::{IpAddr, IpAddr::*, Ipv4Addr},
+	process::Command,
+};
 
 pub fn get_context() -> Result<HashMapContext, EvalexprError> {
 	let time = Local::now();
@@ -14,6 +17,39 @@ pub fn get_context() -> Result<HashMapContext, EvalexprError> {
 		V6(_) => {
 			eprintln!("error could not get ip addr: ipv6 is not supported");
 			Ipv4Addr::new(0, 0, 0, 0)
+		},
+	};
+
+	let router_mac = Command::new("bash")
+		.args(&[
+			"-o",
+			"pipefail",
+			"-c",
+			r#"nmcli d wifi list |grep -E '^\*' | awk '{print $2}'"#,
+		])
+		.output();
+	let router_mac = match router_mac {
+		Ok(out) => {
+			if out.status.success() {
+				let mut mac =String::from_utf8(out.stdout).unwrap_or_else(|err| {
+					eprintln!("error geting wifi.router.mac; bash return non valid utf8: {:?}", err);
+					"".to_owned()
+				});
+				mac.pop();
+				mac
+			} else {
+				{
+					eprintln!(
+						"error geting wifi.router.mac; bash exit with {} \n {:?}",
+						out.status, out.stderr
+					);
+					"".to_owned()
+				}
+			}
+		},
+		Err(error) => {
+			eprintln!("error geting wifi.router.mac; failed to execute bash {:?}", error);
+			"".to_owned()
 		},
 	};
 
@@ -30,6 +66,7 @@ pub fn get_context() -> Result<HashMapContext, EvalexprError> {
 	"ipv4.1" => ip.octets().get(1).unwrap().clone() as i64,
 	"ipv4.2" => ip.octets().get(2).unwrap().clone() as i64,
 	"ipv4.3" => ip.octets().get(3).unwrap().clone() as i64,
+	"wifi.router.mac" => router_mac,
 	}?;
 	println!("contex: {:#?}", contex);
 	Ok(contex)
